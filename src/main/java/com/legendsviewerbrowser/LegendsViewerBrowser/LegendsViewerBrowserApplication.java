3193aeb6d91e6971f8b50b2bcbcec49bdf005c1b
bbenefield89 <bsquared18@gmail.com>
2019-03-16 14:16:21 -0400

    INITIAL

---
+++ src/main/java/com/legendsviewerbrowser/LegendsViewerBrowser/LegendsViewerBrowserApplication.java
@@ -0,0 +1,13 @@
+package com.legendsviewerbrowser.LegendsViewerBrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendsViewerBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendsViewerBrowserApplication.class, args);
+	}
+
+}
